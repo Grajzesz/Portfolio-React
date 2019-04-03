559bbfe1e224ee408a9cdbc44eb3ef199922d75e
Mariusz Buskiewicz <grajzesz@gmail.com>
2019-04-03 13:54:59 +0200

    oki

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import FirstPage from './FirstPage';
 import Footer from './Footer';
-import '../src/css/reset.css'; // Import regular stylesheet
-import '../src/css/main.css'; // Import regular stylesheet
+import '../src/css/reset.css';
+import '../src/css/main.css';
 
 class App extends Component {
     render() {
