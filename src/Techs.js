3313c0c369bafed0693875efedde8108c2b90b7d
Mariusz Buskiewicz <grajzesz@gmail.com>
2019-04-05 10:57:40 +0200

    oki

--- src/Techs.js
+++ src/Techs.js
@@ -34,6 +34,45 @@ const Techs = () => {
                     </div>
                 </div>
             </div>
+            <div className="school__container">
+                <div className="school__container-inner">
+                    <div className="school__coders-main">
+                        <div className="school__coders" />
+                        <div className="school__coders-list" />
+                        <h2 className="school__coders-list-title">
+                            KURS JAVA SCRIPT DEVELOPER REACT
+                        </h2>
+                        <ul>
+                            <li>JavaScript</li>
+                            <li>React</li>
+                            <li>SASS</li>
+                            <li>jQuery</li>
+                            <li>ECMAScript6</li>
+                            <li>Git</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>RWD</li>
+                        </ul>
+                    </div>
+                    <div className="school__alx-main">
+                        <div className="school__alx" />
+                        <div className="school__alx-list" />
+                        <h2 className="school__alx-list-title">
+                            TWORZENIE STRON WWW
+                        </h2>
+                        <ul>
+                            <li>HTML i CSS</li>
+                            <li>Przygotowywanie grafiki na potrzeby WWW</li>
+                            <li>Budowanie struktury strony WWW</li>
+                            <li>
+                                Podstawy programowania w JavaScriptprzy pomocy
+                                biblioteki jQuery
+                            </li>
+                            <li>Praca z systemem CMS Wordpress</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
