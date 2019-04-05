9b5b3d610dae221efffc65a02611583ab7fd0ef0
Mariusz Buskiewicz <grajzesz@gmail.com>
2019-04-05 09:53:23 +0200

    yeah

--- src/Techs.js
+++ src/Techs.js
@@ -6,32 +6,33 @@ const Techs = () => {
         <section>
             <div className="tech__container">
                 <div className="tech__container-inner">
-                    <div className="htmle" data-text="FrontEndDev">
+                    <div className="htmle">
                         <i className="fab fa-html5" />
                     </div>
-                    <div className="sassy" data-text="FrontEndDev">
+                    <div className="sassy">
                         <i className="fab fa-sass" />
                     </div>
-                    <div className="cssy" data-text="FrontEndDev">
+                    <div className="cssy">
                         <i className="fab fa-css3-alt" />
                     </div>
 
-                    <div className="reacty" data-text="FrontEndDev">
+                    <div className="reacty">
                         <i className="fab fa-react" />
                     </div>
-                    <div className="jscripty" data-text="FrontEndDev">
+                    <div className="jscripty">
                         <i className="fab fa-js-square" />
                     </div>
-                    <div className="jqueryy" data-text= {<img src="https://img.icons8.com/ios/50/000000/jquery-filled.png" />}>
+                
+                    <div className="jqueryy">
                         <img src="https://img.icons8.com/ios/50/000000/jquery-filled.png" />
-                    </div><div className="gity" data-text="FrontEndDev">
+                    </div><div className="gity">
                         <i className="fab fa-git-square" />
 
                     </div>
-                    <div className="nody" data-text="FrontEndDev">
+                    <div className="nody">
                         <i className="fab fa-node" />
                     </div>
-                    <div className="wordpressy" data-text="FrontEndDev">
+                    <div className="wordpressy">
                         <i className="fab fa-wordpress" />
                     </div>
                 </div>
