2027544ab48ab5786ab9d7dd5ce91e46f03b7800
Mariusz Buskiewicz <grajzesz@gmail.com>
2019-04-10 10:10:46 +0200

    ok

--- src/Contact.js
+++ src/Contact.js
@@ -1,30 +1,45 @@
 import React from 'react';
 const Contact = () => {
     return (
-        <section className="formularz"><form>
-        <div className="box">
-        <h1>Formularz kontaktowy :</h1>
-        <label>
+        <section className="formularz">
+            <div className="form__container-title">Contact</div>
+            <form>
+                <div className="box">
+                    {/* <h1>Formularz kontaktowy :</h1> */}
+                    {/* <label>
         <span>Nazwa użytkownika</span>
         <input type="text" className="wpis" name="nazwa" id="nazwa"/>
-        </label>
-        <label>
-        <span>E-mail :</span>
-        <input type="text" className="wpis" name="email" id="email"/>
-        </label>
-        <label>
-        <span>Temat :</span>
-        <input type="text" className="wpis" name="temat" id="temat"/>
-        </label>
-        <label>
-        <span>Wiadomość :</span>
-        <textarea className="wiadomosc" name="wiadomosc" id="tresc"></textarea>
-        <input type="button" className="button" value="Wyślj" />
-        </label>
-        </div>
-        </form></section>
-
-
+        </label> */}
+                    <label>
+                        <span>E-mail :</span>
+                        <input
+                            type="text"
+                            className="wpis"
+                            name="email"
+                            id="email"
+                        />
+                    </label>
+                    <label>
+                        <span>Temat :</span>
+                        <input
+                            type="text"
+                            className="wpis"
+                            name="temat"
+                            id="temat"
+                        />
+                    </label>
+                    <label>
+                        <span>Wiadomość :</span>
+                        <textarea
+                            className="wiadomosc"
+                            name="wiadomosc"
+                            id="tresc"
+                        />
+                    </label>
+                    <input type="button" className="button" value="Wyślj" />
+                </div>
+            </form>
+        </section>
     );
 };
 
